ce97bf236e7cb5f6f41b06712d2abc8632d85dae
Nikita Kozlov <nikita@stroika.io>
2019-03-31 22:11:31 -0700

    Completed a few more challenges

---
+++ challenges/leetcode/maximum_subarray.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * 
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * 
+ * 
+ */
+var maxSubArray = function(nums) {
+
+
+    var prev = 0;
+    var max = -Infinity;
+  
+    for (var i = 0; i < nums.length; i++) {
+      prev = Math.max(prev + nums[i], nums[i]);
+      max = Math.max(max, prev);
+    }
+    return max;
+
+
+
+    
+};
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
+i = 8
+prev = 6
+max = 6
